b2d0c69b139d06364d5ecfb860a113362d3e7fa9
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-02 23:19:44 +0200

    head or tail

--- python_practice/PythonProjects/bmi.py
+++ python_practice/PythonProjects/bmi.py
@@ -1,23 +1,39 @@
 """
+BMI Calculator Program
+
+This program calculates Body Mass Index (BMI) based on user input for weight and height. It then categorizes the BMI into different weight status levels according to commonly used standards.
+
+Usage:
+1. Run the program.
+2. Input your weight in kilograms when prompted.
+3. Input your height in square meters when prompted.
+4. The program calculates the BMI and provides a weight status level.
+
+Note: BMI is calculated as weight (in kg) divided by the
+square of height (in meters).
 
 """
-weight = input("Enter Your Weight (KG): ")
-height = input("Enter Your Height (m^2): ")
 
+# Get input for weight and height from the user
+weight = input("Enter Your Weight (KG): ")
+height = input("Enter Your Height (m): ")
+
+# Calculate BMI
 bmi = float(weight) / float(height)
 
+# Categorize BMI into different weight status levels
 if bmi < 18.5:
-    print("Your BMI is {}".format(bmi))
+    print("Your BMI is {:.2f}".format(bmi))
     print("You are Underweight")
-elif bmi >= 18.5 and bmi <= 24.9:
-    print("Your BMI is {}".format(bmi))
+elif 18.5 <= bmi <= 24.9:
+    print("Your BMI is {:.2f}".format(bmi))
     print("You are Normal Weight")
-elif bmi >= 25.0 and bmi <= 29.9:
-    print("Your BMI is {}".format(bmi))
-    print("You are Overweight(Pre-Obese)")
-elif bmi >= 30.0 and bmi <= 34.9:
-    print("Your BMI is {}".format(bmi))
-    print("You are Obese(Class 1)")
-elif bmi >= 35.0 and bmi <= 39.9:
-    print("Your BMI is {}".format(bmi))
-    print("You are Clinically Obese(Class 2)")
+elif 25.0 <= bmi <= 29.9:
+    print("Your BMI is {:.2f}".format(bmi))
+    print("You are Overweight (Pre-Obese)")
+elif 30.0 <= bmi <= 34.9:
+    print("Your BMI is {:.2f}".format(bmi))
+    print("You are Obese (Class 1)")
+elif 35.0 <= bmi <= 39.9:
+    print("Your BMI is {:.2f}".format(bmi))
+    print("You are Clinically Obese (Class 2)")
---
+++ python_practice/food.py
@@ -0,0 +1,19 @@
+"""
+a Food Order program for a local bukateria
+"""
+# prices of meals per plate
+rice = 300
+beans = 250
+spagetthi = 250
+
+# sides
+beans = 100
+spagetthi = 100
+
+# prices of toppings per piece
+meat = 100
+egg = 150
+
+bill = 0
+
+order = input("Place your order: ")
